eeef29d219daf40d9eee20b44d9d9414de61e31c
Tshepiso Setsiba <tsetsiba@bhp.org.bw>
2016-10-04 00:27:47 +0200

    demographics section, in progress

--- amp_dashboard/views/subject_dashboard_view.py
+++ amp_dashboard/views/subject_dashboard_view.py
@@ -6,19 +6,38 @@ from edc_base.views import EdcBaseViewMixin
 from .appointment_visit_crf_view_mixin import AppointmentSubjectVisitCRFViewMixin
 from .locator_results_actions_view_mixin import LocatorResultsActionsViewMixin
 from .markey_view_mixin import MarkeyViewMixin
+from amp.models.screening_consent import ScreeningConsent
 
 
 class SubjectDashboardView(
         MarkeyViewMixin,
         AppointmentSubjectVisitCRFViewMixin, LocatorResultsActionsViewMixin, EdcBaseViewMixin, TemplateView):
 
+    context = {}
     template_name = 'amp_dashboard/subject_dashboard.html'
 
     def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context.update(
+        self.context = super().get_context_data(**kwargs)
+        self.context.update(
             title=settings.PROJECT_TITLE,
             project_name=settings.PROJECT_TITLE,
             site_header=admin.site.site_header,
         )
-        return context
+        self.context.update({
+            'markey_data': self.markey_data,
+            'markey_next_row': self.markey_next_row
+        })
+        return self.context
+
+    @property
+    def consent(self):
+        try:
+            screening_consent = ScreeningConsent.objects.get(subject_identifier=self.subject_identifier)
+        except ScreeningConsent.DoesNotExist:
+            screening_consent = None
+        return screening_consent
+
+    @property
+    def subject_identifier(self):
+        subject_identifier = self.context.get('subject_identifier')
+        return subject_identifier
