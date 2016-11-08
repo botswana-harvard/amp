98360adc4b5a3692637b2d3aaa2e80fc86f3ce37
Coulson <ckgathi@bhp.org.bw>
2016-11-08 09:16:28 +0200

    fix template locations

--- amp/apps.py
+++ amp/apps.py
@@ -67,8 +67,8 @@ class EdcTimepointAppConfig(EdcTimepointAppConfig):
 class EdcLabelAppConfig(EdcLabelAppConfigParent):
     default_cups_server_ip = '192.168.4.145'
     default_printer_label = 'pharma'
-    default_template_folder = os.path.join(settings.STATIC_ROOT, 'templates', 'label_templates')
-    extra_templates_folder = os.path.join(settings.STATIC_ROOT, 'templates', 'label_templates')
+    default_template_folder = os.path.join(settings.BASE_DIR, 'templates', 'static', 'templates', 'label_templates')
+    extra_templates_folder = os.path.join(settings.BASE_DIR, 'templates', 'static', 'templates', 'label_templates')
 
 
 class EdcRegistrationAppConfig(EdcRegistrationAppConfigParent):
