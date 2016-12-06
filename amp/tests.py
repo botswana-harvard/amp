9a2a4ca8005154df400a27b5225c9b53a51e58aa
erikvw <ew2789@gmail.com>
2016-12-06 11:14:40 +0200

    PEP8

--- amp/tests.py
+++ amp/tests.py
@@ -16,8 +16,7 @@ class TestScreeningConsentIdentifierAllocation(TestCase):
     def setUp(self):
         for identifier in ['1001243-1', '1001243-2', '1001243-3', '1001243-4', '1001243-5', '1001243-6']:
             SubjectIdentifier.objects.create(
-                subject_identifier=identifier
-            )
+                subject_identifier=identifier)
 
     def test_identifiers(self):
         self.assertEqual(6, SubjectIdentifier.objects.all().count())
@@ -27,7 +26,6 @@ class TestScreeningConsentIdentifierAllocation(TestCase):
         mommy.make(ScreeningConsent, identity='111121118', confirm_identity='111121118')
         mommy.make(ScreeningConsent, identity='111121117', confirm_identity='111121117')
         self.assertEqual(RegisteredSubject.objects.filter(subject_identifier='1001243-1').count(), 1)
-
         self.assertEqual(ScreeningConsent.objects.filter(subject_identifier='1001243-1').count(), 1)
 
     def test_allocate_identifier2(self):
@@ -62,36 +60,26 @@ class TestScreeningConsentIdentifierAllocation(TestCase):
     def test_allocate_identifier_on_resave(self):
 
         mommy.make(ScreeningConsent, identity='111121116', confirm_identity='111121116')
-
         self.assertEqual(ScreeningConsent.objects.filter(subject_identifier='1001243-1').count(), 1)
-
         screeningconsent = ScreeningConsent.objects.get(subject_identifier='1001243-1')
         screeningconsent.save()
-
         self.assertEqual(SubjectIdentifier.objects.filter(allocated_datetime=None).count(), 5)
 
     def test_allocate_identifier5(self):
 
         mommy.make(ScreeningConsent, identity='111121115', confirm_identity='111121115')
-
         self.assertEqual(ScreeningConsent.objects.filter(subject_identifier='1001243-1').count(), 1)
-
         mommy.make(ScreeningConsent, identity='111121114', confirm_identity='111121114')
-
         self.assertEqual(ScreeningConsent.objects.filter(subject_identifier='1001243-2').count(), 1)
 
     def test_allocate_identifier2_resave(self):
 
         mommy.make(ScreeningConsent, identity='111121113', confirm_identity='111121113')
-
         screeningconsent = ScreeningConsent.objects.get(subject_identifier='1001243-1')
         screeningconsent.save()
-
         mommy.make(ScreeningConsent, identity='111121112', confirm_identity='111121112')
-
         screeningconsent = ScreeningConsent.objects.get(subject_identifier='1001243-2')
         screeningconsent.save()
-
         self.assertEqual(SubjectIdentifier.objects.filter(allocated_datetime=None).count(), 4)
 
 
