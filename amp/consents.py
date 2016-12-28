0ed5eef25b68fcee2ac7c69c01049ffe3d1d6ca9
erikvw <ew2789@gmail.com>
2016-12-28 21:01:14 +0200

    remove redundant requirements

--- amp/consents.py
+++ amp/consents.py
@@ -11,7 +11,7 @@ from edc_constants.constants import MALE, FEMALE
 
 app_config = django_apps.get_app_config('edc_protocol')
 
-subjectconsent_v1 = Consent(
+screeningconsent_v1 = Consent(
     'amp.screeningconsent',
     version='1',
     start=arrow.get(datetime(2016, 5, 1, 0, 0, 0), tzinfo=gettz('Africa/Gaborone')).to('UTC').datetime,
@@ -21,4 +21,4 @@ subjectconsent_v1 = Consent(
     age_max=64,
     gender=[MALE, FEMALE])
 
-site_consents.register(subjectconsent_v1)
+site_consents.register(screeningconsent_v1)
