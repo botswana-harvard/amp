3f7bd96f4a1d3a7837b7ba2692ec7881e001761a
Gofaone Magodi <magodign@magodign.local>
2017-08-31 10:47:15 +0200

    add more visit codes

--- amp/visit_schedules.py
+++ amp/visit_schedules.py
@@ -1,7 +1,8 @@
+from edc_visit_schedule.constants import WEEKS
+from edc_visit_schedule.schedule import Schedule
 from edc_visit_schedule.site_visit_schedules import site_visit_schedules
 from edc_visit_schedule.visit_schedule import VisitSchedule
-from edc_visit_schedule.schedule import Schedule
-from edc_visit_schedule.constants import WEEKS
+
 
 crfs = ()
 
@@ -22,7 +23,9 @@ visit_codes = [
     '1.0', '2.0', '3.0', '4.0', '5.0', '6.0', '7.0', '8.0',
     '9.0', '10.0', '11.0', '12.0', '13.0', '14.0', '15.0',
     '16.0', '17.0', '19.0', '20.0', '21.0', '22.0', '23.0',
-    '24.0', '25.0', '26.0']
+    '24.0', '25.0', '26.0', '31.0', '32.0', '33.0', '34.0',
+    '35.0', '47.0', '48.0', '71.0', '72.0', '73.0', '74.0',
+    '75.0', '76.0', '77.0', '78.0']
 
 schedule = Schedule(name='amp-schedule')
 interval = 0
